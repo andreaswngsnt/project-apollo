7534c27797bcc9165f8952a5c9db74aa77c73eca
andreaswngsnt <40526361+andreaswngsnt@users.noreply.github.com>
2018-06-23 20:33:51 -0700

    Created new routes and updated dashboard interface
    Added permanent and temporary routes to API. Uploaded js plugins. Updated dashboard article form.

--- dY/home/HomeController.js
+++ dY/home/HomeController.js
@@ -21,4 +21,33 @@ router.get("/tentang", function(req, res) {
     res.render("tentang");
 });
 
+// TEMPORARY - REMOVE IF NEEDED
+router.get("/login", function(req, res) {
+	res.render("login");
+});
+
+router.get("/register", function(req, res) {
+	res.render("register");
+});
+
+router.get("/admin", function(req, res) {
+	res.render("dashboard/admin");
+});
+
+router.get("/admin/artikel/index", function(req, res) {
+	res.render("dashboard/artikel/index");
+});
+
+router.get("/admin/artikel/user", function(req, res) {
+	res.render("dashboard/artikel/user");
+});
+
+router.get("/admin/artikel/new", function(req, res) {
+	res.render("dashboard/artikel/new");
+});
+
+router.get("/contoh", function(req, res) {
+	res.render("artikel/show");
+});
+
 module.exports = router;
