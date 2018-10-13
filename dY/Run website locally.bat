a1e4bccbd97bcc196ff015de60b3287d7279a5a2
Tedo Pranowo <tedokdr@yahoo.com>
2018-10-13 11:38:47 +0700

    Implement doctor list page

---
+++ dY/Run website locally.bat
@@ -0,0 +1,8 @@
+rem Run MongoDB database
+start cmd /c "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe"
+
+rem Run Command Prompt in current location
+start cmd
+
+rem Run local server
+node server.js
