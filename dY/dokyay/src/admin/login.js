c65ac3cf91be25cfc3c8d00edbd98651c0333682
Iryanto Jaya <iryanto62@gmail.com>
2018-10-19 16:17:39 +0800

    login dokter

--- dY/dokyay/src/admin/login.js
+++ dY/dokyay/src/admin/login.js
@@ -9,11 +9,11 @@ import List						from '@material-ui/core/List'
 import ListItem	 from '@material-ui/core/ListItem'
 import TextField from '@material-ui/core/TextField'
 
-import DokYayFooter		from '../components/DokYay/DokYayFooter'
-import DokYayHeader		from '../components/DokYay/DokYayHeader'
+import DokYayFooter	 from '../components/DokYay/DokYayFooter'
+import DokYayHeader	 from '../components/DokYay/DokYayHeader'
 import GridContainer from '../components/Grid/GridContainer'
-import GridItem						from '../components/Grid/GridItem'
-import Parallax						from '../components/Parallax/Parallax'
+import GridItem	     from '../components/Grid/GridItem'
+import Parallax	     from '../components/Parallax/Parallax'
 
 import loginPageStyle from '../assets/jss/material-kit-pro-react/views/loginPageStyle'
 import parallaxBg     from '../assets/img/bg/bg1.jpg'
@@ -25,18 +25,28 @@ class Login extends Component {
 	}
 
 	state = {
-		userType: '',
+		userType: '', // 'umum', 'dokter'
 		email:    '',
 		password: ''
 	}
 
 	componentDidMount = () => {
-		// let type = getQueryVariable('type')
-		// console.log(type)
+		function getQueryVariable(param) {
+			let query = window.location.search.substring(1), // eliminate '?'
+				vars = query.split('&')
+			for (let i = 0; i < vars.length; i++) {
+				let pair = vars[i].split('=')
+				if (pair[0] == param) {
+					return pair[1]
+				}
+			}
+		}
+
+		let type = getQueryVariable('type')
 		
 		// Get login user type
 		//
-		this.setState({ userType: 'patient' })
+		this.setState({ userType: type })
 	}
 
 	externalLogin = (platform) => {
@@ -50,113 +60,83 @@ class Login extends Component {
 	}
 
 	submitLogin = () => {
-		alert('Login!')
+		alert('Login' + this.state.userType + ' !')
 	}
 
 	render = () => {
 		const { classes } = this.props
-		let loginForm = null
-
-		if (!this.state.userType) {
-			// Undefined
-			loginForm = (
-				<div style={{ width: '100%', padding: '16px', background: '#eee', color: '#000'}}>
-					Invalid user type
-				</div>
-			)
-		}
-		else if (this.state.userType === 'patient') {
-			loginForm = (
-				<div>
-					<Card style={{
-						width:     '350px',
-						margin:    '0px auto',
-						padding:   '16px',
-						textAlign: 'center'
-					}}>
-						<h2>Log In Umum</h2>
-						Log in dengan media sosial<br/>
-						<br/>
-						<i className='fab fa-facebook-square fa-2x'
-							style={{
-								cursor:      'pointer',
-								marginRight: '16px',
-								color:       '#3B5998'
-							}}
-							onClick={this.externalLogin.bind(this, 'facebook')}></i>
-						<i className='fab fa-twitter-square fa-2x'
-							style={{
-								cursor:      'pointer',
-								marginRight: '16px',
-								color:       '#1DA1F2'
-							}}
-							onClick={this.externalLogin.bind(this, 'twitter')}></i>
-						<i className='fab fa-google-plus-square fa-2x'
-							style={{
-								cursor: 'pointer',
-								color:  '#DB4437'
-							}}
-							onClick={this.externalLogin.bind(this, 'googleplus')}></i><br/>
-						<br/><br/>
-						Log in normal <br/>
-						<i className='fas fa-envelope'
-							style={{
-								display:       'inline-block',
-								marginRight:   '16px',
-								marginBottom:  '12px',
-								verticalAlign: 'bottom'
-							}}></i>
-						<TextField
-							required
-							style={{display: 'inline-block'}}
-							placeholder='Email'
-							value={this.state.email}
-							onChange={this.handleChange('email')} />
-						<br/><br/>
-						<i className='fas fa-lock'
-							style={{
-								display:       'inline-block',
-								marginRight:   '16px',
-								marginBottom:  '12px',
-								verticalAlign: 'bottom'
-							}}></i>
-						<TextField
-							required
-							placeholder='Password'
-							type='password'
-							value={this.state.password}
-							onChange={this.handleChange('password')} />
-						<br/><br/><br/>
-						<Button variant='contained' color='primary' className={classes.button} onClick={this.submitLogin}>
-							Log In
-						</Button>
-						<br/><br/>
-					</Card>
-				</div>
-			)
-		}
-		else if (this.state.userType === 'doctor') {
-			loginForm = (
-				<div style={{ width: '100%', padding: '16px', background: '#eee', color: '#000'}}>
-					Doctor user type
-				</div>
-			)
-		}
-		else {
-			// Uncaught user type
-			loginForm = (
-				<div style={{ width: '100%', padding: '16px', background: '#eee', color: '#000'}}>
-					Uncaught user type
-				</div>
-			)
-		}
+		let loginForm = null,
+			userTypeText = this.state.userType === 'dokter' ? 'Dokter' : 'Umum'
 
 		return (
 			<div style={{ border: '1px solid black', height: '100vh' }}>
 				<DokYayHeader />
 				<div className={classes.main}>
 					<div className={classes.container} style={{color: '#000000'}}>
-						{loginForm}
+						<Card style={{
+							width:     '350px',
+							margin:    '0px auto',
+							padding:   '16px',
+							textAlign: 'center'
+						}}>
+							<h2>Log In {userTypeText}</h2>
+							Log in dengan media sosial<br/>
+							<br/>
+							<i className='fab fa-facebook-square fa-2x'
+								style={{
+									cursor:      'pointer',
+									marginRight: '16px',
+									color:       '#3B5998'
+								}}
+								onClick={this.externalLogin.bind(this, 'facebook')}></i>
+							<i className='fab fa-twitter-square fa-2x'
+								style={{
+									cursor:      'pointer',
+									marginRight: '16px',
+									color:       '#1DA1F2'
+								}}
+								onClick={this.externalLogin.bind(this, 'twitter')}></i>
+							<i className='fab fa-google-plus-square fa-2x'
+								style={{
+									cursor: 'pointer',
+									color:  '#DB4437'
+								}}
+								onClick={this.externalLogin.bind(this, 'googleplus')}></i><br/>
+							<br/><br/>
+							Log in normal <br/>
+							<i className='fas fa-envelope'
+								style={{
+									display:       'inline-block',
+									marginRight:   '16px',
+									marginBottom:  '12px',
+									verticalAlign: 'bottom'
+								}}></i>
+							<TextField
+								required
+								style={{display: 'inline-block'}}
+								placeholder='Email'
+								value={this.state.email}
+								onChange={this.handleChange('email')} />
+							<br/><br/>
+							<i className='fas fa-lock'
+								style={{
+									display:       'inline-block',
+									marginRight:   '16px',
+									marginBottom:  '12px',
+									verticalAlign: 'bottom'
+								}}></i>
+							<TextField
+								required
+								placeholder='Password'
+								type='password'
+								value={this.state.password}
+								onChange={this.handleChange('password')} />
+							<br/><br/><br/>
+							<Button variant='contained' color='primary' className={classes.button} onClick={this.submitLogin}>
+								Log In
+							</Button>
+							<br/><br/>
+						</Card>
 					</div>
 				</div>
 				<DokYayFooter />
